e6d1cb8e1decec5ddf1588673951f2b572f20baa
Kirill <kp406221@gmail.com>
2024-10-07 22:44:47 +0300

    Начал парсить телефонные номера и имена риелторов

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import time
 import random
 import multiprocessing
@@ -11,6 +12,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -60,40 +62,79 @@ def parse_realtors_data(
         time.sleep(1)
 
         # Парсинг ссылок на всех риелторов на странице
-        realtors_links = WebDriverWait(adspower_browser, 5).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "text-box-info-title"))
-        )
+        try:
+            bad_realtors_links = WebDriverWait(adspower_browser, 5).until(
+                EC.presence_of_all_elements_located(
+                    (By.CLASS_NAME, "text-box-info-title")
+                )
+            )
+        except TimeoutException:
+            print("Риелторы по данному региону закончились")
+            current_region_pos += 1
+            continue
+
+        realtors_links = [link.get_attribute("href") for link in bad_realtors_links]
 
         # Фильтруем риелторов по дате регистрации
         realtors_registration_data = WebDriverWait(adspower_browser, 5).until(
             EC.presence_of_all_elements_located((By.CLASS_NAME, "prop-el"))
         )
+
+        i = 0
+        updated = []
         for data in realtors_registration_data:
-            if not data.get_attribute("title").startswith("Дата регистрации"):
+            i += 1
+            if data.get_attribute("title").startswith("Дата регистрации"):
+                updated.append(data)
                 continue
-            if int(data.text[13:17]) < 2017:
-                data_idx = realtors_registration_data.index(data)
-                realtors_links.pop(data_idx)
+
+        if len(realtors_links) == 0:
+            current_region_pos += 1
+            print("Риелторы по региону собраны")
+            if current_region_pos == end_region_pos:
+                logger.success("Все доступные риелторы собраны")
+                break
+
+        new_realtors_links = []
+        for data_idx, data in enumerate(updated):
+            if int(data.text[13:17]) >= 2017:
+                new_realtors_links.append(realtors_links[data_idx])
 
         # Заходим по каждой ссылке и собираем данные по одному конкретному риелтору:
         # 1) Имя
         # 2) Телефон
         # 3) Специализация
         # 4) Регион
-        for link in realtors_links:
-            realtor_link = link.get_attribute("href")
-            adspower_browser.get(realtor_link)
+        for link in new_realtors_links:
+            adspower_browser.get(link)
 
             # Сбор данных со странички
-            # ...
-
+            source = adspower_browser.page_source
+            # print(source)
+            name = None
+            try:
+                name = WebDriverWait(adspower_browser, 1).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "info"))
+                )
+            except:
+                try:
+                    name = WebDriverWait(adspower_browser, 1).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, "realtor__info-name"))
+                    )
+                except:
+                    try:
+                        name = WebDriverWait(adspower_browser, 1).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, "title"))
+                        )
+                    except:
+                        print("Имя не найдено")
+            print(name.text)
+            phone_number = re.findall(r"\+\d{1,3}[-\s]?\(?\d{1,5}\)?[-\s]?\(?\d{1,5}\)?[-\s]?\d{1,5}[-\s]?\d{1,5}[-\s]?\d{1,5}", source)[0]
+            # phone_number = WebDriverWait(adspower_browser, 1).until(
+            #     EC.presence_of_element_located((By.CLASS_NAME, "phone"))
+            # )
+            print(phone_number)
             time.sleep(DELAY)
-
-        if realtors_links == []:
-            current_region_pos += 1
-            if current_region_pos == end_region_pos:
-                logger.success("Все доступные риелторы собраны")
-                break
 
         current_page_idx += 1
         time.sleep(1)
@@ -111,7 +152,7 @@ if __name__ == "__main__":
             ADSPOWER_ID1,
             ADSPOWER_NAME1,
             region_idxs,
-            0,
+            73,
             REGION_IDXS_AMOUNT // 2,
             adspower_driver,
         ],
